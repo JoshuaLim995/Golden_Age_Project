597e931158a9eb26f2419e6425e7f5d746920eb9
Joshua <limshiawjong@gmail.com>
2017-10-19 22:23:32 +0800

    Added create patient and client layout

---
+++ GoldenAge/app/src/main/java/com/joshua_lsj/goldenage/AddClientActivity.java
@@ -0,0 +1,10 @@
+package com.joshua_lsj.goldenage;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by limsh on 10/19/2017.
+ */
+
+public class AddClientActivity extends AppCompatActivity {
+}
