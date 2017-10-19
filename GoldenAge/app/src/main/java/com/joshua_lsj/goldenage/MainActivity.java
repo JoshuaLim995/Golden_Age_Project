b889aadb6f9caaf43964e403d1229e80095d8a82
Joshua <limshiawjong@gmail.com>
2017-10-19 23:54:53 +0800

    Added custom floating menu with sub button from a github source https://github.com/Clans/FloatingActionButton

--- GoldenAge/app/src/main/java/com/joshua_lsj/goldenage/MainActivity.java
+++ GoldenAge/app/src/main/java/com/joshua_lsj/goldenage/MainActivity.java
@@ -1,13 +1,15 @@
 package com.joshua_lsj.goldenage;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.design.widget.FloatingActionButton;
+//import com.github.clans.fab.FloatingActionButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
     @Override
