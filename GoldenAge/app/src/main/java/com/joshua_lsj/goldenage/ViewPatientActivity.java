1acfaa4bf6ed01b54811f4f4fab801a5b206dac6
Joshua <limshiawjong@gmail.com>
2017-10-21 00:56:30 +0800

    Added view patient skeleton

---
+++ GoldenAge/app/src/main/java/com/joshua_lsj/goldenage/ViewPatientActivity.java
@@ -0,0 +1,11 @@
+package com.joshua_lsj.goldenage;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by limsh on 10/21/2017.
+ */
+
+public class ViewPatientActivity extends AppCompatActivity {
+
+}
