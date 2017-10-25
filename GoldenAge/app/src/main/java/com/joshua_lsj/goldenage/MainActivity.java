98d8cdf12f35ee62a0a5001e4b3f38397a15b594
Joshua <limshiawjong@gmail.com>
2017-10-25 23:17:20 +0800

    Login alert when open app

--- GoldenAge/app/src/main/java/com/joshua_lsj/goldenage/MainActivity.java
+++ GoldenAge/app/src/main/java/com/joshua_lsj/goldenage/MainActivity.java
@@ -1,8 +1,13 @@
 package com.joshua_lsj.goldenage;
 
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -21,6 +26,8 @@ import android.view.MenuItem;
 
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -29,8 +36,7 @@ import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 //import android.support.design.widget.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ListView listView;
     private static final String FRAGMENT_ADD_PATIENT = "com.addPatientFragment";
@@ -39,9 +45,16 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+
+        //Hide the Floating Button
+        FloatingActionMenu fam = (FloatingActionMenu) findViewById(R.id.fam);
+        fam.setVisibility(View.INVISIBLE);
+
+        /*
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -58,9 +71,47 @@ public class MainActivity extends AppCompatActivity
                 .replace(R.id.content_frame, new ListViewPatientFragment())
                 .commit();
         getSupportActionBar().setTitle("Patients");
+*/
 
-
+        showAlert();
     }
+
+public void showAlert(){
+    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+    LayoutInflater li = LayoutInflater.from(this);
+    View promptsView = li.inflate(R.layout.prompts, null);
+
+    alertDialogBuilder.setView(promptsView);
+
+    final EditText userInput = (EditText) promptsView
+            .findViewById(R.id.editTextDialogUserInput);
+
+
+    // set dialog message
+    alertDialogBuilder
+            .setCancelable(false)
+            .setPositiveButton("OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,int id) {
+                            // get user input and set it to result
+                            // edit text
+                            if(userInput.getText().toString() == "LOL")
+                                Toast.makeText(getApplicationContext(),
+                                        "Login Sucessfull", Toast.LENGTH_LONG).show();
+                            else
+                                Toast.makeText(getApplicationContext(),
+                                        "NO", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+
+    // create alert dialog
+    AlertDialog alertDialog = alertDialogBuilder.create();
+
+    // show it
+    alertDialog.show();
+}
 
     public void onFloatingActionButtonClicked(View view){
         Intent intent;
