2416e64737878fe42311e23ce647e18cd562997f
JoshuaLim995 <limshiawjong@gmail.com>
2017-11-17 08:32:44 +0800

    Add upload image

--- API/PatientRegister.php
+++ API/PatientRegister.php
@@ -11,50 +11,57 @@ if(isset($_GET['apicall'])){
 //if it is an upload call we will upload the image
 		case 'signup':
 //first confirming that we have the image and tags in the request parameter
-		if(isset($_FILES['pic']['name']) && isset($_POST['tags'])){
-//uploading file and storing it to database as well 
+		if(isset($_FILES['pic']['name']) && isTheseParametersAvailable(array('name','ic','Birthyear','gender','bloodType','address','contact','meals','allergic','sickness','regType','regDate','margin'))){
+
+			$name = $_POST['name'];
+			$ic = $_POST['ic'];
+			$Birthyear = $_POST['Birthyear'];
+			$gender = $_POST['gender'];
+			$bloodType = $_POST['bloodType'];
+			$address = $_POST['address'];
+			$contact = $_POST['contact'];
+			$meals = $_POST['meals'];
+			$allergic = $_POST['allergic'];
+			$sickness = $_POST['sickness'];
+			$regType = $_POST['regType'];
+			$regDate = $_POST['regDate'];
+			$margin = $_POST['margin'];
+			
+			$image = $_FILES['pic']['name'];
+			$image_temp = $_FILES['pic']['tmp_name'];
+			
 			try{
-				move_uploaded_file($_FILES['pic']['tmp_name'], UPLOAD_PATH . $_FILES['pic']['name']);
-				$stmt = $conn->prepare("INSERT INTO images (image, tags) VALUES (?,?)");
-				$stmt->bind_param("ss", $_FILES['pic']['name'],$_POST['tags']);
+				//Check if is not null
+				if(!empty($image)){
+					//uploading file 
+					move_uploaded_file($image_temp, UPLOAD_PATH . $image);
+				}
+				else if(empty($image)){
+					$image = 'NULL';
+				}
+				
+				//Store into database
+				$stmt = $conn->prepare("INSERT INTO patients (name, ic, Birthyear, gender, bloodType, address, contact, meals, allergic, sickness, regType, regDate, margin, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			$stmt->bind_param("ssssssssssssss", $name, $ic, $Birthyear, $gender, $bloodType, $address, $contact, $meals, $allergic, $sickness, $regType, $regDate, $margin, $image);
+
+				
 				if($stmt->execute()){
 					$response['error'] = false;
-					$response['message'] = 'File uploaded successfully';
+					$response['message'] = 'Patient registered successfully';
 				}else{
-					throw new Exception("Could not upload file");
+					
+					throw new Exception("Could not register patient");
 				}
 			}catch(Exception $e){
 				$response['error'] = true;
-				$response['message'] = 'Could not upload file';
+				$response['message'] = 'Could not register patient';
 			}
 		}else{
 			$response['error'] = true;
 			$response['message'] = "Required params not available";
 		}
 		break;
-//in this call we will fetch all the images 
-		case 'getpics':
-//getting server ip for building image url 
-		$server_ip = gethostbyname(gethostname());
-//query to get images from database
-		$stmt = $conn->prepare("SELECT id, image, tags FROM images");
-		$stmt->execute();
-		$stmt->bind_result($id, $image, $tags);
-		$images = array();
-
-//fetching all the images from database
-//and pushing it to array 
-		while($stmt->fetch()){
-			$temp = array();
-			$temp['id'] = $id; 
-			$temp['image'] = 'http://' . $server_ip . '/Android/'. UPLOAD_PATH . $image; 
-			$temp['tags'] = $tags; 
-			array_push($images, $temp);
-		}
-//pushing the array in response 
-		$response['error'] = false;
-		$response['images'] = $images; 
-		break; 
+			
 		default: 
 		$response['error'] = true;
 		$response['message'] = 'Invalid api call';
@@ -68,5 +75,15 @@ if(isset($_GET['apicall'])){
 //displaying the response in json 
 header('Content-Type: application/json');
 echo json_encode($response);
+
+
+function isTheseParametersAvailable($params){
+	foreach($params as $param){
+		if(!isset($_POST[$param])){
+			return false; 
+		}
+	}
+	return true; 
+}
 
 ?>
